4cd4850e3a00ac9babec6b691a240b4c4fd97094
Abhijit Parlapalli <abhijit.parla01@gmail.com>
2024-10-22 23:53:44 +0530

    validations and api added

--- app/Onboarding/calorie-calculator-form.tsx
+++ app/Onboarding/calorie-calculator-form.tsx
@@ -3,36 +3,63 @@ import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
 import { RadioGroup, RadioGroupItem } from '@/components/ui/radio-group'
-import React from 'react'
+import { headers } from 'next/headers'
+import React, { useState } from 'react'
 
 const CalorieCalculatorForm = () => {
-    const handleSubmit = (e: any) => {
+    const [age, setAge] = useState("")
+    const [gender, setGender] = useState("")
+    const [heightft, setHeightft] = useState()
+    const [heightinches, setHeightinches] = useState()
+    const [heightcms, setHeightcms] = useState("")
+    const [weightkg, setWeightkg] = useState("")
+    const [weightpounds, setWeightpounds] = useState()
+    const handleSubmit = async (e: any) => {
         e.preventDefault()
+        console.log(e, age, gender, heightcms, weightkg)
+        const response = await fetch('/api/calculate_calories',{
+            method: "GET",
+            headers: {
+                age,
+                gender,
+                heightcms,
+                weightkg
+            }
+        }
+        )
+        const data = await response.json()
     }
     return (
         <div className='max-w-2xl mx-auto'>
             <form onSubmit={handleSubmit}>
                 <div className='m-4'>
                     <Label className='text-slate-50'>Age:</Label>
-                    <Input placeholder='age' type='number' className='mt-2'/>
+                    <Input placeholder='age' required min={0} type='number' className='mt-2' onChange={(e: any) => setAge(e.target.value)} value={age}/>
                 </div>
                 <div className='m-4'>
                     <Label className='text-slate-50'>Gender:</Label>
-                    <RadioGroup defaultValue="option-one" className='flex'>
+                    <RadioGroup defaultValue={gender} value={gender} required className='flex' onValueChange={(e: any) => {
+                        console.log(e)
+                        setGender(e)
+                    }}>
                         <div className="flex items-center space-x-2 m-3">
-                            <RadioGroupItem value="option-one" id="option-one" />
-                            <Label htmlFor="option-one">Male</Label>
+                            <RadioGroupItem value="male" id="option-one" />
+                            <Label>Male</Label>
                         </div>
                         <div className="flex items-center space-x-2 m-3">
-                            <RadioGroupItem value="option-two" id="option-two" />
-                            <Label htmlFor="option-two">Female</Label>
+                            <RadioGroupItem value="female" id="option-two" />
+                            <Label>Female</Label>
                         </div>
                     </RadioGroup>
 
                 </div>
                 <div className='m-4'>
                     <Label className='text-slate-50 mb-3'>Height:</Label>
-                    <Input placeholder='height in cms' type="number" className='mt-2' />
+                    <Input required min={0} placeholder='height in cms' type="number" className='mt-2' value={heightcms} onChange={(e: any) => setHeightcms(e.target.value)} />
+                </div>
+                <div className='m-4'>
+                    <Label className='text-slate-50 mb-3'>Weight:</Label>
+                    <Input required min={0} placeholder='Weight in Kilos' type="number" className='mt-2' value={weightkg} onChange={(e: any) => setWeightkg(e.target.value)}/>
                 </div>
                 <div className='flex justify-center m-4'>
                     <Button type='submit' className='w-2/3 self-center'>Calculate</Button>
--- app/api/calculate_calories/route.ts
+++ app/api/calculate_calories/route.ts
@@ -1,6 +1,13 @@
 import { NextRequest, NextResponse } from "next/server";
 
-export default function GET (request: NextRequest, response: NextResponse) {
+export async function GET (request: NextRequest, response: NextResponse) {
+    const headers = request.headers;
+    const age = headers.get('age')
+    const gender = headers.get('gender')
+    const height = headers.get('heightcms')
+    const weight = headers.get('weightkg')
+    console.log(age, gender, height, weight)
+    // fetch()
     return NextResponse.json({
         message:"ok"
     })
